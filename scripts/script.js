4caccfe00fa36b46041d0cf2d089d799d1c9195c
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-01-12 21:40:12 -0500

    added clues and functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,27 @@
 // Contenedor de la palabra y palabras para el juego
 const contenedor = document.querySelector('.contenedor__letras'), // Contenedor de la palabra
-palabras = ['gato', 'juego', 'plato', 'camino', 'jirafa', 'cooperar', 'programar', 'elefante', 'camilla', 'robot', 'trofeo', 'martillo'], // Array de palabras a encontrar
 abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], //Abecedario para poner los botones
 contenedorAbc = document.getElementById('abc'); //Contenedor padre del teclado
 
+let palabras = [
+    {word: 'gato', clue: 'Pista: Gato'},
+    {word: 'juego', clue: 'Pista: juego'},
+    {word: 'plato', clue: 'Pista: plato'},
+    {word: 'camino', clue: 'Pista: camino'},
+    {word: 'jirafa', clue: 'Pista: jirafa'},
+    {word: 'cooperar', clue: 'Pista: cooperar'},
+    {word: 'programar', clue: 'Pista: programar'},
+    {word: 'elefante', clue: 'Pista: elefante'},
+    {word: 'camilla', clue: 'Pista: camilla'},
+    {word: 'robot', clue: 'Pista: robot'},
+    {word: 'trofeo', clue: 'Pista: trofeo'},
+    {word: 'martillo', clue: 'Pista: martillo'},
+]; // Array de palabras a encontrar
+
 let palabra = palabras[Math.floor((Math.random() * palabras.length))], // Obtener palabra random de la lista
+palabraFinal = palabra.word;
 posiciones = document.getElementsByClassName('letra'); // Recuardros de las letras
-vidas = 7,// vidas
+vidas = 0,// vidas
 aciertos = 0; // Variables de aciertos
 
 // Dibuja los botones con las letras
@@ -15,9 +30,9 @@ for(let abcedario in abc){
 }
 
 // Dibuja las lineas en pantalla
-console.log(palabra);
+console.log(palabraFinal);
 
-for(let p in palabra){
+for(let p in palabraFinal){
     contenedor.innerHTML += `<div class="letra"></div>`;
 }
 
@@ -40,16 +55,20 @@ function quitarVidas(i) {
     let indices = i;
     // Quita vidas cuando se equivocan de palabra 
     if(indices.length == 0){
-        vidas--;
-        document.getElementById('ahorcado').innerHTML = `<img src="img/${vidas}.svg" class="img-ahorcado">`
+        vidas++;
+        if(window.innerWidth < 1025){
+            document.querySelector('#ahorcado').style.backgroundPosition = `${-(191.7*vidas)}px`;
+        }else{
+            document.querySelector('#ahorcado').style.backgroundPosition = `${-(314.55 *vidas)}px`;
+        }
     }
 }
 
 // Verifica cuantas vidas hay y acabar o no el juego
 function verificarVidas() {
     // Si las vidas se acaban pierde y se quita el teclado para que no agregue más letras
-    if(vidas == 0){
-        document.getElementById('gameover').innerText = 'Perdiste';
+    if(vidas == 7){
+        document.getElementById('gameover').innerText = `Perdiste la palabra era ${palabraFinal.toLocaleUpperCase()}`;
         contenedorAbc.innerHTML = null;
     }
 }
@@ -69,11 +88,11 @@ function verificarAciertos(i) {
     let indices = i;
     if(indices.length != 0){
         aciertos += indices.length;
-        if(aciertos == palabra.length){ // Si los aciertos sin iguales al tamaño de la palabra gana
+        if(aciertos == palabraFinal.length){ // Si los aciertos sin iguales al tamaño de la palabra gana
             document.getElementById('gameover').innerText = 'Ganaste';
-            document.getElementById('ahorcado').innerHTML = `<img src="img/win.svg">`;
+            document.getElementById('ahorcado').style.backgroundImage = `url("img/win.svg")`;
+            document.getElementById('ahorcado').style.backgroundSize = `cover`;
             contenedorAbc.innerHTML = null;
-            // document.getElementById('ahorcado').innerHTML = null;
         }
     }
 }
@@ -82,12 +101,12 @@ function buscarLetra(l){
     //Buscar el indice donde se encuentra la letra
     let letra = l,
     indices = [],
-    indice = palabra.indexOf(letra);
+    indice = palabraFinal.indexOf(letra);
 
     if(letra.length == 1){
         while(indice != -1){
             indices.push(indice);
-            indice = palabra.indexOf(letra, indice + 1);
+            indice = palabraFinal.indexOf(letra, indice + 1);
         }
         return indices;
     }else{
@@ -101,4 +120,8 @@ function quitarBoton(id) {
     if(boton != null){
         boton.style.display = 'none';
     }
+}
+
+function showClue() {
+    alert(palabra.clue);
 }
